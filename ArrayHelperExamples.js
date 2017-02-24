717bffddd470cd344f0505f72645a52234100ae5
PcCodeWarrior <PC_Code_Warrior@yahoo.com>
2017-02-24 15:13:19 -0700

    adjusted lines

--- ArrayHelperExamples.js
+++ ArrayHelperExamples.js
@@ -209,9 +209,9 @@ console.log(unique);
 function balanceParens(string){
     return !string.split('').reduce((previous,char ) =>{
             if (previous<0){return previous};
-    if (char ==='('){return ++previous};
-    if (char ===')'){return --previous};
-    return previous;
+            if (char ==='('){return ++previous};
+            if (char ===')'){return --previous};
+            return previous;
 },0);
 }
 
